05e7c516da0df8f529c03d38aedf58848bdcfee5
EspenLunden <espenlunden81@gmail.com>
2025-11-13 18:46:19 -0500

    made dashboard new home directory, json storing and initial reading working)

--- test.py
+++ test.py
@@ -9,22 +9,6 @@ classPath = "user_classes.json"
 
 name = "TEMP_USER"
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    #posts
-    if fl.request.method == 'POST':
-
-        #redirect to logins
-        if 'to_login' in fl.request.form:
-            return fl.redirect(fl.url_for('login'))
-        
-        #test
-        elif 'other_button' in fl.request.form:
-            print("You clicked the other button!")
-        
-    #template
-    return fl.render_template('home.html', person=name)
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     #posts
@@ -41,7 +25,7 @@ def login():
     #template
     return fl.render_template('login.html')
 
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def dashboard():
     username = fl.session.get('username', 'TEMP_USER')
     return fl.render_template('dashboard.html', person=username)
@@ -58,18 +42,39 @@ def createClass():
             # write to json
             with open(classPath, 'w') as file:
                 className = fl.request.form.get("class_name")
-                userClasses[className] = {}
+                userClasses[className] = {"fields": []}
+                
+                # Parse dynamic fields from form data
+                # Fields are named as fields[0][name], fields[0][type], fields[1][name], etc.
+                field_indices = set()
                 for key in fl.request.form:
-                    if key == "fields[][name]":
-                        userClasses[className]["fields"] = []
-                        for i in range(len(fl.request.form.getlist(key))):
-                            field = {}
-                            field["name"] = fl.request.form.getlist(key)[i]
-                            field["type"] = fl.request.form.getlist("fields[][type]")[i]
-                            userClasses[className]["fields"].append(field)
-                    if key != "class_name" and key != "submit":
-                        userClasses[className][key] = fl.request.form.get(key)
+                    if key.startswith("fields["):
+                        # Extract the index from keys like "fields[0][name]"
+                        index_str = key.split("[")[1].split("]")[0]
+                        field_indices.add(int(index_str))
+                
+                # Build fields array in order
+                for i in sorted(field_indices):
+                    field_name_key = f"fields[{i}][name]"
+                    field_type_key = f"fields[{i}][type]"
+
+                    # Add default ID field
+                    field = {
+                            "name": "ID",
+                            "type": "number"
+                        }
+                    userClasses[className]["fields"].append(field)
+                    
+                    # Add user-defined fields
+                    if field_name_key in fl.request.form and field_type_key in fl.request.form:
+                        field = {
+                            "name": fl.request.form.get(field_name_key),
+                            "type": fl.request.form.get(field_type_key)
+                        }
+                        userClasses[className]["fields"].append(field)
+                    
                 json.dump(userClasses, file, indent=4)
+                print(f"Successfully created class '{className}' with {len(userClasses[className]['fields'])} fields")
         else:
             print("Class name already exists or no name provided.")
     
