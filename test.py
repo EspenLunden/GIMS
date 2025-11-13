8fc38388d84c9897b40b4f6b0592411590a6d80e
EspenLunden <espenlunden81@gmail.com>
2025-11-13 18:28:25 -0500

    working on storing user defined gear classes

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import flask as fl
+import json
+import os
 
 app = fl.Flask(__name__)
 app.secret_key = "dev-secret-key"  # needed for session management
+userClasses = {}
+classPath = "user_classes.json"
 
 name = "TEMP_USER"
 
@@ -44,10 +48,41 @@ def dashboard():
 
 @app.route('/create_class', methods=['GET', 'POST'])
 def createClass():
-    #posts
+    # posts
+    if fl.request.method == 'POST':
+        # create class submit, check if class name already exists
+        if 'submit' in fl.request.form and fl.request.form.get("class_name") not in userClasses:
+            print("Creating class...")
+            print(fl.request.form)
+
+            # write to json
+            with open(classPath, 'w') as file:
+                className = fl.request.form.get("class_name")
+                userClasses[className] = {}
+                for key in fl.request.form:
+                    if key == "fields[][name]":
+                        userClasses[className]["fields"] = []
+                        for i in range(len(fl.request.form.getlist(key))):
+                            field = {}
+                            field["name"] = fl.request.form.getlist(key)[i]
+                            field["type"] = fl.request.form.getlist("fields[][type]")[i]
+                            userClasses[className]["fields"].append(field)
+                    if key != "class_name" and key != "submit":
+                        userClasses[className][key] = fl.request.form.get(key)
+                json.dump(userClasses, file, indent=4)
+        else:
+            print("Class name already exists or no name provided.")
     
     #template
-    return fl.render_template('createClass.html')
+    return fl.render_template('create_class.html')
 
-if __name__ == '__main__':  
-   app.run(debug = True)
+if __name__ == '__main__':
+   # load user classes if file exists
+    if os.path.exists(classPath) and os.path.getsize(classPath) > 0:
+        with open(classPath, 'r') as file:
+           userClasses = json.load(file)
+    else:
+        userClasses = {}
+
+    # run app
+    app.run(debug = True)
